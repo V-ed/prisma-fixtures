f556e31987ec20c9f60a9771626fbd2fdfbb8939
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2022-06-29 18:33:06 -0400

    fix: update deps to use working versions and fix vitest not working
    this now builds using pkgroll to use a proper build management tool
    
    vite's damn ssr.noExternal strikes again, which requires yet again the use of ts-ignore... arrrg

--- tests/.vitest/vitest.config.ts
+++ tests/.vitest/vitest.config.ts
@@ -1,13 +1,17 @@
 import VitePluginTsConfigPaths from 'vite-tsconfig-paths';
 import { defineConfig, UserConfig } from 'vitest/config';
 
-type CoverageReporter = UserConfig['test']['coverage']['reporter'];
+type CoverageReporter = NonNullable<NonNullable<UserConfig['test']>['coverage']>['reporter'];
 
-const coverageReporter: CoverageReporter = (process.env.COV_REPORTER as CoverageReporter) ?? 'lcov';
+const coverageReporter: NonNullable<CoverageReporter> = (process.env.COV_REPORTER as CoverageReporter) ?? 'lcov';
 const coverageDirectory: string = process.env.COV_DIRECTORY ?? `./coverage`;
 
 const vitestBaseConfig = defineConfig({
 	plugins: [VitePluginTsConfigPaths({ loose: true })],
+	// @ts-ignore
+	ssr: {
+		noExternal: ['class-importer'],
+	},
 	test: {
 		testTimeout: 30000,
 		passWithNoTests: true,
